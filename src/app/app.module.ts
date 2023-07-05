36b4cb600981def4a02fddd3a955e973eaa8c721
JoacoNavarre <joaquinnavarre@gmail.com>
2023-07-05 10:42:43 -0300

    primeros maquetados del proyecto CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PagesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
